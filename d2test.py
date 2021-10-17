720158253105c8874efaf7f792326696bfc99d47
Jeremy Ruroede <jeremyruroede@gmail.com>
2021-10-17 18:54:20 -0400

    Update d2test.py

--- d2test.py
+++ d2test.py
@@ -1,10 +1,19 @@
 import os
 
-def install_function():
-  #instance segmentation
-    os.system('python demo.py --config-file ../configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml --input ./frame7.jpg --opts MODEL.WEIGHTS detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl')
-
+def test():
+  #instance segmentation 
+  os.system('python demo.py --config-file ../configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml --input ./my_file.jpg --opts MODEL.WEIGHTS detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl')
+  
+  #panotic segmentation
+    os.system('python demo.py --config-file ../configs/COCO-PanopticSegmentation/panoptic_fpn_R_50_3x.yaml --input ./my_file.jpg --opts MODEL.WEIGHTS detectron2://COCO-PanopticSegmentation/panoptic_fpn_R_50_3x/139514569/model_final_c10459.pkl')
+  
+  #object detection
+  os.system('python demo.py --config-file ../configs/COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml --input ./my_file.jpg --opts MODEL.WEIGHTS detectron2://COCO-Detection/fast_rcnn_R_50_FPN_1x/137635226/model_final_e5f7ce.pkl')
    
-
-
-install_function()
+  #Keypoint
+  os.system('python demo.py --config-file ../configs/COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml --input ./my_file.jpg --opts MODEL.WEIGHTS detectron2://COCO-Keypoints/keypoint_rcnn_R_50_FPN_1x/137261548/model_final_04e291.pkl')
+  
+  #DensePose
+  os.system('python apply_net.py show configs/densepose_rcnn_R_50_FPN_s1x.yaml https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl C:/full/path/to/my_file.jpg dp_contour,bbox -v')
+            
+test()
