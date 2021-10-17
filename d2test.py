a57d5556d32b582ce8cbc3a5e2a633430d57d957
Jeremy Ruroede <jeremyruroede@gmail.com>
2021-10-17 16:16:25 -0400

    Update d2test.py

--- d2test.py
+++ d2test.py
@@ -1,4 +1,10 @@
 import os
-import sys 
+
+def install_function():
+  #instance segmentation
+    os.system('python demo.py --config-file ../configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml --input ./frame7.jpg --opts MODEL.WEIGHTS detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl')
+
+   
 
 
+install_function()
